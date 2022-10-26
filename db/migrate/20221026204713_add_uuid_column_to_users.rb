f02559263e7b4673149f3873645632d223128392
Emre1555 <emreyuksel4253@gmail.com>
2022-10-26 23:50:02 +0300

    uuid

---
+++ db/migrate/20221026204713_add_uuid_column_to_users.rb
@@ -0,0 +1,5 @@
+class AddUuidColumnToUsers < ActiveRecord::Migration[7.0]
+  def change
+    add_column :users, :uuid, :uuid, default: "gen_random_uuid()", null: false
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,8 +10,9 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_10_24_200522) do
+ActiveRecord::Schema[7.0].define(version: 2022_10_26_204713) do
   # These are extensions that must be enabled in order to support this database
+  enable_extension "pgcrypto"
   enable_extension "plpgsql"
 
   create_table "comments", force: :cascade do |t|
@@ -64,6 +65,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_10_24_200522) do
     t.datetime "remember_created_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.uuid "uuid", default: -> { "gen_random_uuid()" }, null: false
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
