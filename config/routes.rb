f686b290c59285e63cb6782d160fb171c8d21aeb
Emre1555 <emreyuksel4253@gmail.com>
2022-11-13 23:54:02 +0300

    views

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,7 @@ Rails.application.routes.draw do
   resources :friendships
   resources :friends, only: [:index, :destroy]
   resources :likes, only:[ :create, :destroy]
-  resources :users, only: [:index, :show, :new, :create, :edit, :update, :destroy]
+  resources :users
   resources :posts do
     resources :comments
   end
